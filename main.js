b9c68b76f65ea62812d7ca6f475c48a91cf2b6a6
lianwei <lianwei@microsoft.com>
2016-05-23 17:27:14 +0800

    Add multiple data source

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // -- prepare: common utils
 var $ = function (selector) {
-	return typeof selector === "string" ? document.querySelector(selector) : selector;
+	return typeof selector === "string" ? document.querySelectorAll(selector) : selector;
 };
 
 $.get = function (url, onload) {
@@ -16,10 +16,10 @@ var searchEngine = function (_) {
 	// _cache is mapping from character to pinyin, possible to have multiple pinyin
 	var dbFile = _.dbFile || "database/PinyinMapping.json",
 		allowMultiPronounce = _.allowMultiPronounce || false;
-		_cache = {},
-		_db = [],
+	_cache = {},
+		_db = {},
 		inited = false;
-		
+
 	_.init = function (callback) {
 		if (inited) {
 			if (callback) callback(_);
@@ -76,32 +76,41 @@ var searchEngine = function (_) {
 					pinyinSentences = product(pinyinSentences, pinyins);
 				}
 			}
-			
-			if (!allowMultiPronounce){
+
+			if (!allowMultiPronounce) {
 				pinyinSentences = pinyinSentences.slice(0, 1);
 			}
-			
+
 			if (callback) callback(pinyinSentences);
 		})
 	}
 
-	_.index = function (dataset, callback) {
-		_.init(function () {
-			dataset.forEach(function (data) {
-				_.convert(data.question, function (pinyins) {
-					pinyins.forEach(function (pinyin) {
-						_db.push({
-							key: pinyin,
-							data: data,
-						});
-					}, this);
-				});
-			}, this);
-			if (callback) callback(_index);
-		});
+	_.index = function (dataset, label, callback) {
+		label = label || "default";
+		if (!_db.hasOwnProperty(label)) {
+			_db[label] = [];
+			_.init(function () {
+				dataset.forEach(function (data) {
+					_.convert(data.question, function (pinyins) {
+						pinyins.forEach(function (pinyin) {
+							_db[label].push({
+								key: pinyin,
+								data: data,
+							});
+						}, this);
+					});
+				}, this);
+				if (callback) callback(_db[label]);
+			});
+		} else {
+			if (callback) callback(_db[label]);
+		}
 	}
-	_.search = function (key) {
-		return _db.filter(function (data) {
+
+	_.search = function (key, label) {
+		label = label || "default";
+		_db[label] = _db[label] || [];
+		return _db[label].filter(function (data) {
 			return data.key.indexOf(key) > -1;
 		}).map(function (data) {
 			return data.data;
@@ -111,45 +120,93 @@ var searchEngine = function (_) {
 	return _;
 } (searchEngine || {});
 
-
-var templateEngine = function (_) {
+var dataCache = function (_) {
 	var cached = {};
-	_.render = function (url, model, callback) {
+	_.get = function (url, callback) {
 		if (cached.hasOwnProperty(url)) {
-			var template = cached[key];
-			var rendered = Mustache.render(template, model);
-			if (callback) callback(rendered);
+			var content = cached[url];
+			if (callback) callback(content);
 		} else {
-			$.get(url, function (template) {
-				Mustache.parse(template);
-				var rendered = Mustache.render(template, model);
-				if (callback) callback(rendered);
+			$.get(url, function (content) {
+				if (callback) callback(content);
 			})
 		}
+	}
+	return _;
+} (dataCache || {});
+
+var templateEngine = function (_) {
+	_.render = function (url, model, callback) {
+		dataCache.get(url, function (template) {
+			Mustache.parse(template);
+			var rendered = Mustache.render(template, model);
+			if (callback) callback(rendered);
+		});
 	};
-	
+
 	return _;
 } (templateEngine || {});
 
-$.get("data.json", function (content) {
-	var data = JSON.parse(content).map(function (raw) {
-		return {
-			question: raw.q,
-			answer: raw.a,
-		}
-	});
+var dataEngine = function (_) {
+	var cached = {};
+	_.load = function (url, callback) {
+		dataCache.get(url, function (content) {
+			if (!cached.hasOwnProperty(url)) {
+				var data = JSON.parse(content).map(function (raw) {
+					return {
+						question: raw.q,
+						answer: raw.a,
+					}
+				});
 
-	searchEngine.index(data);
-});
+				searchEngine.index(data, url);
+				cached[url] = data;
+			}
+
+			callback(cached[url]);
+		});
+	}
+	return _;
+} (dataEngine || {});
 
 window.onload = function () {
-	var input = $("#search-box-input");
+	var input = $("#search-box-input")[0];
 	input.addEventListener('input', function () {
 		var val = input.value;
-		if (val.length < 3) return;
-		var results = searchEngine.search(val);
-		templateEngine.render("templates/result.mustache", {results: results}, function(html){
-			$("#search-group-content").innerHTML = html;
-		})
+		search(val);
 	});
+
+	var selectors = $('.data-source-selector');
+	for (var i = 0; i < selectors.length; i++) {
+		activeClick(selectors[i]);
+	}
+
+	function search(val) {
+		if (val.length < 3) return;
+		var url = $('.active>.data-source-selector')[0].innerText;
+		dataEngine.load(url, function (content) {
+			var results = searchEngine.search(val, url);
+			templateEngine.render("templates/result.mustache", { results: results }, function (html) {
+				$("#search-group-content")[0].innerHTML = html;
+			})
+		});
+	}
+
+	function activeClick(element) {
+		element.addEventListener('click', function () {
+			setActive(element.parentElement.parentElement, element.parentElement);
+		});
+	}
+
+	function setActive(parentElement, activeElement) {
+		var children = parentElement.children;
+		for (var i = 0; i < children.length; i++) {
+			if (children[i] === activeElement) {
+				children[i].classList.add('active');
+			}
+			else {
+				children[i].classList.remove('active');
+			}
+		}
+	}
 };
